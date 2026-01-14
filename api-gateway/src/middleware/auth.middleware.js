7f420dda40328dbb545f9347b01a579e00cf8f6e
ARYAN242526 <roy2503aryan@gmail.com>
2026-01-14 22:00:48 +0530

    fix : create post error resolved

--- api-gateway/src/middleware/auth.middleware.js
+++ api-gateway/src/middleware/auth.middleware.js
@@ -2,7 +2,7 @@ import logger from "../utils/logger.js";
 import jwt from 'jsonwebtoken';
 
 const validateToken = (req,res,next) => {
-    const authHeader = req.header['authorisation'];
+    const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(" ")[1];
 
     if(!token){
--- post-service/src/middleware/auth.middleware.js
+++ post-service/src/middleware/auth.middleware.js
@@ -4,7 +4,7 @@ const authenticateRequest = (req , res, next) => {
     const userId = req.headers['x-user-id'];
 
     if(!userId){
-        logger.warn(`Access attgempted without user ID`);
+        logger.warn(`Access attempted without user ID`);
         return res.status(401).json({
             success : false,
             message : "Authentication required! Please login to continue"
